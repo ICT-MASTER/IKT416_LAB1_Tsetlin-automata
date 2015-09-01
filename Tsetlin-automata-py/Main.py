2a16a2075b7b71151c6f7639a66fbd81be9cf6f7
Per-Arne Andersen <perara12@gmail.com>
2015-09-01 15:53:23 +0200

    Cleanup 2+3 compability

--- Tsetlin-automata-py/Main.py
+++ Tsetlin-automata-py/Main.py
@@ -1,97 +1,49 @@
+
+# Libraries
 import random
 import uuid
 import sys
 import operator
+import sys, getopt
 import os
-
-class Automata(object):
-
-
-    def __init__(self, states):
-
-        # Do not allow odd numbers (Should never happen unless 3.5 example)
-        if (states * 2) %  2 != 0:
-            raise ValueError("Cannot use a odd number")
-
-        self.states = states
-        self.state = random.randint(self.states, (self.states) + 1)
-        self.id = uuid.uuid1()
-        # print("Automata: {0} - Init state: {1}").format(self.id, self.state)
+import time
 
 
-        self.tmpyes = 0
-        self.tmpno = 0
+# Internal classes
+from Automata import Automata
+from Environment import Environment
 
 
-    def isYes(self):
-        total_states = self.states * 2
-        if self.state > (total_states / 2):
-            return True
-        else:
-            return False
-
-
-    def reward(self):
-
-        total_states = self.states * 2
-
-        # Positive side
-        if self.state > (total_states / 2):
-            #print ("Reward {0} --> {1}").format(self.state, min(self.state + 1, total_states))
-            self.state = min(self.state + 1, total_states)
-        # Negative side
-        else:
-            #print ("Reward {0} --> {1}").format(self.state, max(self.state - 1, 1))
-            self.state = max(self.state - 1, 1)
-
-    def punish(self):
-        total_states = self.states * 2
-        # Positive side
-        if self.state > (total_states / 2):
-            #print ("Punish {0} --> {1}").format(self.state, self.state - 1)
-            self.state = self.state - 1
-        # Negative side
-        else:
-            #print ("Punish {0} --> {1}").format(self.state, self.state + 1)
-            self.state = self.state + 1
-
-
-
-class Environment(object):
-
-    def __init__(self, yes, num_automatas):
-        self.yes = yes # M
-        self.num_automatas = num_automatas
-        self.prob = 0
-
-
-    def probability(self):
-
-        #percent_yes = (self.yes / self.num_automatas) * 100.0
-
-        # Above 60% YES
-        if self.yes <= 3:
-            self.prob = self.yes * 0.2  # yes=5, probl =1 ... yes=0, prob = 0,
-        else:
-            self.prob = 0.6 - (self.yes - 3) * 0.2  # yes=5, 0.6-2*0.2 = 0.5
-
-
-    def doReward(self):
-
-        r_num = random.uniform(0.0, 1.0)
-
-        if r_num >= self.prob:
-            return False
-        else:
-            return True
-
-
-
-# Logic ----------------
-states = int(sys.argv[1])
+# Initial argument parsing
+states = 3
 num_automata = 5
-iterations = int(sys.argv[3])
-times =  int(sys.argv[2])
+iterations = 1000
+times = 1000
+
+refresh_rate = 1000 # 1 second
+isDone = False # If calculation is done
+
+
+arguments = sys.argv[1:]
+
+if len(arguments) > 0:
+    states = arguments[0]
+
+if len(arguments) > 1:
+    num_automata = arguments[1]
+
+if len(arguments) > 2:
+    iterations = arguments[2]
+
+if len(arguments) > 3:
+    times = arguments[3]
+
+print("Mega advanced automata is initiating...")
+print("Using following parameters:")
+print("-------")
+print("States: {0}\nAutomatas: {1}\nIterations: {2}\nLoops: {3}".format(states, num_automata, iterations, times))
+print("-------")
+print("Progress: ")
 
 # Create automatas
 automata_list = [Automata(states) for x in range(num_automata)]
@@ -101,38 +53,33 @@ result = {}
 for x in range(times):
     for x in range(iterations):
 
-    # Count YES
+        # Count yes for each of the automatas
         yes = 0
         for automata in automata_list:
-            if automata.isYes():
-                automata.tmpyes = automata.tmpyes + 1 # TODO
-                yes = yes + 1
-            else:
-                automata.tmpno = automata.tmpno + 1 # TODO
-
+            yes = yes + 1 if automata.isYes() else yes
 
     # Create environment and calculate probability
     env = Environment(yes, len(automata_list))
     env.probability()
 
+    # Determine weither to punish or reward automatas
     for automata in automata_list:
-        if env.doReward():# and False:
-            #print "Rewarding"
-            automata.reward()
-        else:
-            #print "Punishing"
-            automata.punish()
+        automata.reward() if env.doReward() else automata.punish()
 
-
-    # Generate result for iteration
+    # Generate key for Dict result (IE: 3/2)
     result_key = "{0}/{1}".format(yes, len(automata_list) - yes)
-    if result_key not in result:
-        result[result_key] = 1
-    else:
-        result[result_key] = result[result_key] + 1
 
-    open('data.dat', 'w').close()
+    # Ensure that the key exist, init if not.
+    result[result_key] = 1 if result_key not in result else result[result_key] + 1
+
+    out = ""
     for key, value in sorted(result.items(), key=operator.itemgetter(1), reverse=True):
+        out = out + "[" + (key+ ": " + str(value)) + "]\t"
+
+    print(out, end="\r")
+
+    #open('data.dat', 'w').close()
+    #for key, value in sorted(result.items(), key=operator.itemgetter(1), reverse=True):
     #print("{0}: {1}").format(key, value)
-        with open ('data.dat', 'a') as f: f.write (key+ "   " + str(value) + '\n')
+    #    with open ('data.dat', 'a') as f: f.write (key+ "   " + str(value) + '\n')
         #os.system("python termgraph.py data.dat")
